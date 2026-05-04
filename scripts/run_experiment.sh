36781bcac2b7810a4b141a8ce206e75e7f83a29b
David P. Federl <david-peter.federl@uni-due.de>
2026-05-04 10:23:53 +0200

    fix: update naming scheme

--- scripts/run_experiment.sh
+++ scripts/run_experiment.sh
@@ -7,7 +7,13 @@ echo ""
 echo "Running experiment: $CONFIG_FILE"
 echo ""
 
+EXPERIMENT_LOG_FOLDER="logs/$(date +%s)/${CONFIG_FILE%.yaml}"
+
 uv run main.py \
   --config "$CONFIG_DIR/$CONFIG_FILE" \
   --resume \
-  --log-dir logs/"${CONFIG_FILE%.yaml}"
+  --log-dir "${EXPERIMENT_LOG_FOLDER}"
+
+uv run src/tools/generate_graphs.py \
+  --input-dir "${EXPERIMENT_LOG_FOLDER}/training/metrics/validation" \
+  --output-dir "${EXPERIMENT_LOG_FOLDER}/training/graphs/validation"
