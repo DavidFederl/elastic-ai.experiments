efaf7daf20eab36c57672e5ecb56b98c73d32dea
David P. Federl <david-peter.federl@uni-due.de>
2026-05-11 14:05:19 +0200

    feat: add delta training

---
+++ experiments/training/mlp_delta_fashionmnist_adam.py
@@ -0,0 +1,103 @@
+import logging
+from datetime import datetime, timezone
+from pathlib import Path
+from random import randint
+from typing import Annotated, Literal
+
+import typer
+from elasticai.creator.nn.delta_compression import DeltaType
+from torch import optim
+from torch.nn.modules import CrossEntropyLoss
+
+from src.nn.data import (
+    fashionmnist_trainingset_flattened,
+    fashionmnist_validationset_flattened,
+    get_dataloader,
+)
+from src.nn.model import linear_v1_delta
+from src.nn.training import TrainingBuilder, set_initial_seed
+from src.tools.generate_graphs import generate_graphs
+from src.utils import setup_logging
+
+logger = logging.getLogger(__name__)
+app = typer.Typer()
+
+
+@app.command()
+def main(
+    log_dir: Annotated[Path, typer.Option()] = Path(
+        f"logs/{datetime.now(tz=timezone.utc).timestamp() * 10000}"
+    ),
+    verbose: Annotated[bool, typer.Option(is_flag=True)] = False,
+    epochs: Annotated[int, typer.Option(min=1)] = 100,
+    batch_size: Annotated[
+        int,
+        typer.Option(
+            min=0, help="samples per batch for dataloader (0 == whole dataset)"
+        ),
+    ] = 0,
+    seed: Annotated[int, typer.Option(min=0, max=(2**32) - 1)] = randint(
+        0, (2**32) - 1
+    ),
+    total_fixed_point_bits: Annotated[int, typer.Option(min=2)] = 8,
+    fraction_bits: Annotated[int, typer.Option(min=1)] = 4,
+    delta_bits: Annotated[int, typer.Option(min=1)] = 4,
+    delta_offset: Annotated[int, typer.Option(min=0)] = 2,
+    delta_type: DeltaType = DeltaType.CONSECUTIVE,
+    device: Literal["cpu", "mps", "cuda"] = "cpu",
+) -> None:
+    setup_logging(log_dir, verbose)
+    set_initial_seed(seed, make_determenistic=True)
+
+    training_ds = fashionmnist_trainingset_flattened()
+    training_dl = get_dataloader(
+        training_ds,
+        batch_size=len(training_ds) if batch_size == 0 else batch_size,
+    )
+
+    validation_ds = fashionmnist_validationset_flattened()
+    validation_dl = get_dataloader(
+        validation_ds,
+        batch_size=len(training_ds) if batch_size == 0 else batch_size,
+    )
+
+    model_id, model = linear_v1_delta(
+        in_features=training_ds[0][0].shape.numel(),
+        out_features=len(training_ds.classes),
+        fixed_point_total_bits=total_fixed_point_bits,
+        fixed_point_fraction_bits=fraction_bits,
+        delta_bit_width=delta_bits,
+        delta_offset=delta_offset,
+        delta_type=delta_type,
+        bias=False,
+    )
+
+    optimizer = optim.Adam(
+        params=model.parameters(),
+        lr=0.001,
+    )
+    loss_fn = CrossEntropyLoss()
+    training = (
+        TrainingBuilder()
+        .dataset(training_ds.classes, training_dl, validation_dl)
+        .model(model)
+        .device(device)
+        .optimizer(optimizer)
+        .loss_fn(loss_fn)
+        .log_dir(log_dir)
+        .build()
+    )
+    training.train(epochs=epochs, skip=True)
+
+    generate_graphs(
+        input_dir=log_dir.joinpath("metrics", "validation"),
+        output_dir=log_dir.joinpath("graphs", "validation"),
+    )
+    generate_graphs(
+        input_dir=log_dir.joinpath("metrics", "training"),
+        output_dir=log_dir.joinpath("graphs", "training"),
+    )
+
+
+if __name__ == "__main__":
+    app()
